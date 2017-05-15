c05d5ae0ec9dce682cc59151756e8b53499e2559
banusion <banusion69@free.fr>
2017-05-15 17:49:51 +0200

    Login session, owner for bucketlist and update test with user of course

--- api/tests.py
+++ api/tests.py
@@ -3,16 +3,22 @@ from .models import Bucketlist
 from rest_framework.test import APIClient
 from rest_framework import status
 from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
 
 
-# Define this after the ModelTestCase
 class ViewTestCase(TestCase):
     """Test suite for the api views."""
 
     def setUp(self):
         """Define the test client and other test variables."""
+        user = User.objects.create(username="banusion")
+
+        # Initialize client and force it to use authentication
         self.client = APIClient()
-        self.bucketlist_data = {'name': 'Go to Ibiza'}
+        self.client.force_authenticate(user=user)
+
+        # Since user model instance is not serializable, use its Id/PK
+        self.bucketlist_data = {'name': 'Go to Ibiza', 'owner': user.id}
         self.response = self.client.post(
             reverse('create'),
             self.bucketlist_data,
@@ -22,11 +28,17 @@ class ViewTestCase(TestCase):
         """Test the api has bucket creation capability."""
         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
 
+    def test_authorization_is_enforced(self):
+        """Test that the api has user authorization."""
+        new_client = APIClient()
+        res = new_client.get('/bucketlists/', kwargs={'pk': 1}, format="json")
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
     def test_api_can_get_a_bucketlist(self):
         """Test the api can get a given bucketlist."""
-        bucketlist = Bucketlist.objects.get()
+        bucketlist = Bucketlist.objects.get(id=1)
         response = self.client.get(
-            reverse('details'),
+            '/bucketlists/',
             kwargs={'pk': bucketlist.id}, format="json")
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -34,6 +46,7 @@ class ViewTestCase(TestCase):
 
     def test_api_can_update_bucketlist(self):
         """Test the api can update a given bucketlist."""
+        bucketlist = Bucketlist.objects.get()
         change_bucketlist = {'name': 'Something new'}
         res = self.client.put(
             reverse('details', kwargs={'pk': bucketlist.id}),
@@ -48,7 +61,6 @@ class ViewTestCase(TestCase):
             reverse('details', kwargs={'pk': bucketlist.id}),
             format='json',
             follow=True)
-
         self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
 
 class ModelTestCase(TestCase):
@@ -56,8 +68,9 @@ class ModelTestCase(TestCase):
 
     def setUp(self):
         """Define the test client and other test variables."""
-        self.bucketlist_name = "Write world class code"
-        self.bucketlist = Bucketlist(name=self.bucketlist_name)
+        user = User.objects.create(username="banusion")
+        self.name = "Write world class code"
+        self.bucketlist = Bucketlist(name=self.name, owner=user)
 
     def test_model_can_create_a_bucketlist(self):
         """Test the bucketlist model can create a bucketlist."""
